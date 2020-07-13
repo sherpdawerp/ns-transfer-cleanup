e6cbf3506a27e919375e7b225590f2a6f862dde1
Andrew <50899169+abrow425@users.noreply.github.com>
2020-07-13 21:37:03 +1000

    Create transfer-cleanup-vars.py

---
+++ transfer-cleanup-vars.py
@@ -0,0 +1,9 @@
+puppets = {
+  "puppet_name_1": "password",
+  "puppet_name_2": "password",
+  "puppet_name_3": "password"
+}
+
+main_name = "main_nation_name"
+
+transfer_card = ["transfer_card_id", "transfer_card_season"]
