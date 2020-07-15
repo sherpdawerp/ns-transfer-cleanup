a37709c8e5c281a78ca073c4ec647da316a4b337
Andrew <50899169+abrow425@users.noreply.github.com>
2020-07-15 15:23:19 +1000

    Update transfer-cleanup.py

--- transfer-cleanup.py
+++ transfer-cleanup.py
@@ -1,4 +1,4 @@
-from nspywrapper import nsRequests, FailedRequest
+from nspywrapper import *
 from .transfer_cleanup_vars import *
 
 main_name = main_name.replace(" ", "_").lower()
@@ -9,7 +9,7 @@ agent = main_name + " , using SherpDaWerp's transfer cleanup tool"
 nsapi = nsRequests(agent)
 
 request = nsapi.world(shards=["card", "owners"], parameters={"cardid": transfer_card[0], "season": transfer_card[1]})
-owners = request[1]["CARD"]["OWNERS"]
+owners = request.data["CARD"]["OWNERS"]
 owners_list = [owner["OWNER"] for owner in owners]
 
 gift_queue = [owner for owner in owners_list if owner in puppets.keys()]
@@ -17,45 +17,45 @@ gift_queue = [owner for owner in owners_list if owner in puppets.keys()]
 for pup_name in gift_queue:
     params = {"cardid": transfer_card[0], "season": transfer_card[1], "to": main_name, "mode": "prepare"}
 
-    try:
-        prepare = nsapi.command(nation=pup_name, command="giftcard", parameters=params, auth=(puppets[pup_name], "", ""))
-    except FailedRequest as err:
-        print("An error was encountered preparing a gift from "+pup_name)
+    prepare = nsapi.command(nation=pup_name, command="giftcard", parameters=params, auth=(puppets[pup_name], "", ""))
+
+    if prepare.status != 200:
+        print("An error was encountered preparing a gift from " + pup_name + ". Error Code: +" + str(prepare.status))
         continue
 
     try:
-        x_autologin = prepare[0]["X-autologin"]
-        x_pin = prepare[0]["X-pin"]
-    except KeyError:
+        x_autologin, x_pin = prepare.get_auth()
+    except MissingHeaders:
         x_autologin = ""
         x_pin = ""
 
     try:
-        token = prepare[1]["NATION"]["SUCCESS"]
+        token = prepare.data["NATION"]["SUCCESS"]
     except KeyError:
         try:
-            message = prepare[1]["NATION"]["ERROR"]
+            message = prepare.data["NATION"]["ERROR"]
         except KeyError:
-            message = prepare[1]
+            message = prepare.data
 
         print(message + " (" + pup_name + ")")
         continue
 
     params = {"cardid": transfer_card[0], "season": transfer_card[1], "to": main_name, "mode": "execute", "token": token}
 
-    try:
-        execute = nsapi.command(nation=pup_name, command="giftcard", parameters=params, auth=(puppets[pup_name], x_autologin, x_pin))
-    except FailedRequest as err:
-        print("An error was encountered making a gift from "+pup_name)
+    execute = nsapi.command(nation=pup_name, command="giftcard", parameters=params, auth=(puppets[pup_name], x_autologin, x_pin))
+
+    if execute.status != 200:
+        print("An error was encountered preparing a gift from " + pup_name + ". Error Code: +" + str(execute.status))
         continue
 
     try:
-        message = prepare[1]["NATION"]["SUCCESS"]
+        print(execute.data["NATION"])
+        message = execute.data["NATION"]["SUCCESS"]
     except KeyError:
         try:
-            message = prepare[1]["NATION"]["ERROR"]
+            message = execute.data["NATION"]["ERROR"]
         except KeyError:
-            message = prepare[1]
+            message = execute.data
 
     print(message + " (" + pup_name + ")")
 
