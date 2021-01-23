5a8b4227e895b6e712f1b7586ca8eddd48788725
dithpri <dithpri@gmail.com>
2021-01-23 10:06:35 +0100

    Add support for multiple transfer cards

--- transfer_cleanup.py
+++ transfer_cleanup.py
@@ -20,7 +20,11 @@ from nspywrapper import *
 
 main_name = "main_nation_name"
 
-transfer_card = ["transfer_card_id", "transfer_card_season"]
+transfer_card = [
+        ["transfer_card1_id", "transfer_card1_season"],
+        # ["transfer_card2_id", "transfer_card2_season"],
+        # etc.
+]
 
 puppets = {
   "puppet_name_1": "password",
@@ -34,72 +38,74 @@ main_name = main_name.replace(" ", "_").lower()
 agent = main_name + " , using SherpDaWerp's transfer cleanup tool"
 
 nsapi = nsRequests(agent)
-request = nsapi.world(shards=["card", "owners"], parameters={"cardid": transfer_card[0], "season": transfer_card[1]})
-owners = request.data["CARD"]["OWNERS"]
-owners_list = [owner["OWNER"] for owner in owners]
 
-puppets = {new_key.replace(" ", "_").lower(): puppets[new_key] for new_key in puppets.keys()}
-gift_queue = [owner for owner in owners_list if owner in puppets.keys()]
+for transfer_card in transfer_cards:
+    request = nsapi.world(shards=["card", "owners"], parameters={"cardid": transfer_card[0], "season": transfer_card[1]})
+    owners = request.data["CARD"]["OWNERS"]
+    owners_list = [owner["OWNER"] for owner in owners]
 
-prev_pup = None
-for pup_name in gift_queue:
-    if pup_name == main_name:
-        continue
-    params = {"cardid": transfer_card[0], "season": transfer_card[1], "to": main_name, "mode": "prepare"}
+    puppets = {new_key.replace(" ", "_").lower(): puppets[new_key] for new_key in puppets.keys()}
+    gift_queue = [owner for owner in owners_list if owner in puppets.keys()]
 
-    prepare = nsapi.command(
-        nation=pup_name,
-        command="giftcard",
-        parameters=params,
-        auth=(
-            puppets[pup_name],
-            x_autologin if prev_pup == pup_name else "",
-            x_pin if prev_pup == pup_name else "",
-        ),
-    )
+    prev_pup = None
+    for pup_name in gift_queue:
+        if pup_name == main_name:
+            continue
+        params = {"cardid": transfer_card[0], "season": transfer_card[1], "to": main_name, "mode": "prepare"}
 
-    if prepare.status != 200:
-        print("An error was encountered preparing a gift from " + pup_name + ". Error Code: +" + str(prepare.status))
-        continue
+        prepare = nsapi.command(
+            nation=pup_name,
+            command="giftcard",
+            parameters=params,
+            auth=(
+                puppets[pup_name],
+                x_autologin if prev_pup == pup_name else "",
+                x_pin if prev_pup == pup_name else "",
+            ),
+        )
 
-    try:
-        x_autologin, x_pin = prepare.get_auth()
-    except MissingHeaders:
-        if prev_pup != pup_name:
-            x_autologin = ""
-            x_pin = ""
-            prev_pup = -1
-    else:
-        prev_pup = pup_name
+        if prepare.status != 200:
+            print("An error was encountered preparing a gift from " + pup_name + ". Error Code: +" + str(prepare.status))
+            continue
 
-    try:
-        token = prepare.data["NATION"]["SUCCESS"]
-    except KeyError:
         try:
-            message = prepare.data["NATION"]["ERROR"]
+            x_autologin, x_pin = prepare.get_auth()
+        except MissingHeaders:
+            if prev_pup != pup_name:
+                x_autologin = ""
+                x_pin = ""
+                prev_pup = -1
+        else:
+            prev_pup = pup_name
+
+        try:
+            token = prepare.data["NATION"]["SUCCESS"]
         except KeyError:
-            message = prepare.data
+            try:
+                message = prepare.data["NATION"]["ERROR"]
+            except KeyError:
+                message = prepare.data
+
+            print(message + " (" + pup_name + ")")
+            continue
+
+        params = {"cardid": transfer_card[0], "season": transfer_card[1], "to": main_name, "mode": "execute", "token": token}
+
+        execute = nsapi.command(nation=pup_name, command="giftcard", parameters=params, auth=(puppets[pup_name], x_autologin, x_pin))
+
+        if execute.status != 200:
+            print("An error was encountered preparing a gift from " + pup_name + ". Error Code: +" + str(execute.status))
+            continue
+
+        try:
+            print(execute.data["NATION"])
+            message = execute.data["NATION"]["SUCCESS"]
+        except KeyError:
+            try:
+                message = execute.data["NATION"]["ERROR"]
+            except KeyError:
+                message = execute.data
 
         print(message + " (" + pup_name + ")")
-        continue
-
-    params = {"cardid": transfer_card[0], "season": transfer_card[1], "to": main_name, "mode": "execute", "token": token}
-
-    execute = nsapi.command(nation=pup_name, command="giftcard", parameters=params, auth=(puppets[pup_name], x_autologin, x_pin))
-
-    if execute.status != 200:
-        print("An error was encountered preparing a gift from " + pup_name + ". Error Code: +" + str(execute.status))
-        continue
-
-    try:
-        print(execute.data["NATION"])
-        message = execute.data["NATION"]["SUCCESS"]
-    except KeyError:
-        try:
-            message = execute.data["NATION"]["ERROR"]
-        except KeyError:
-            message = execute.data
-
-    print(message + " (" + pup_name + ")")
 
 print("\nCompleted!")
