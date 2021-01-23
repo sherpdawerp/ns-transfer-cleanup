b863ad06998e99f6dac864c564a32e7795df0010
dithpri <dithpri@gmail.com>
2021-01-23 10:04:00 +0100

    Correctly handle multiple gifts from the same nation

--- transfer_cleanup.py
+++ transfer_cleanup.py
@@ -18,15 +18,15 @@ from nspywrapper import *
 
 # # # # # BITS TO BE EDITED BELOW HERE # # # # #
 
+main_name = "main_nation_name"
+
+transfer_card = ["transfer_card_id", "transfer_card_season"]
+
 puppets = {
   "puppet_name_1": "password",
   "puppet_name_2": "password",
   "puppet_name_3": "password"
 }
-
-main_name = "main_nation_name"
-
-transfer_card = ["transfer_card_id", "transfer_card_season"]
 
 # # # # # BITS TO BE EDITED ABOVE HERE # # # # #
 
@@ -41,10 +41,22 @@ owners_list = [owner["OWNER"] for owner in owners]
 puppets = {new_key.replace(" ", "_").lower(): puppets[new_key] for new_key in puppets.keys()}
 gift_queue = [owner for owner in owners_list if owner in puppets.keys()]
 
+prev_pup = None
 for pup_name in gift_queue:
+    if pup_name == main_name:
+        continue
     params = {"cardid": transfer_card[0], "season": transfer_card[1], "to": main_name, "mode": "prepare"}
 
-    prepare = nsapi.command(nation=pup_name, command="giftcard", parameters=params, auth=(puppets[pup_name], "", ""))
+    prepare = nsapi.command(
+        nation=pup_name,
+        command="giftcard",
+        parameters=params,
+        auth=(
+            puppets[pup_name],
+            x_autologin if prev_pup == pup_name else "",
+            x_pin if prev_pup == pup_name else "",
+        ),
+    )
 
     if prepare.status != 200:
         print("An error was encountered preparing a gift from " + pup_name + ". Error Code: +" + str(prepare.status))
@@ -53,8 +65,12 @@ for pup_name in gift_queue:
     try:
         x_autologin, x_pin = prepare.get_auth()
     except MissingHeaders:
-        x_autologin = ""
-        x_pin = ""
+        if prev_pup != pup_name:
+            x_autologin = ""
+            x_pin = ""
+            prev_pup = -1
+    else:
+        prev_pup = pup_name
 
     try:
         token = prepare.data["NATION"]["SUCCESS"]
